b85e83634eab6ae3403906852fe488e3e2b83b20
AleksandraKlosowski <1612760@students.wits.ac.za>
2022-06-02 23:31:55 +0200

    add inspo board

---
+++ src/helpers/WebArtInspoList.js
@@ -0,0 +1,21 @@
+import WebInspo from '../assets/Web.jpg';
+import WebInspo2 from '../assets/Web2.png';
+import WebInspo3 from '../assets/Web3.png';
+
+
+
+export const WireframeListTwo = [
+    {
+        name: "Inspiration1",
+        image: WebInspo,
+    },
+    {
+        name: "Inspiration2",
+        image: WebInspo2,
+    },
+    {
+        name: "Inspiration3",
+        image: WebInspo3,
+    },
+
+];
