f9a52c8fb416e010b9d32ad1bf2d4be54d72b044
AleksandraKlosowski <1612760@students.wits.ac.za>
2022-06-02 21:45:29 +0200

    added css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import './index.css';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
