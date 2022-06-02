0f18f0257b6894b009c7e6363e3ce773c3449a23
AleksandraKlosowski <1612760@students.wits.ac.za>
2022-06-02 21:47:00 +0200

    txtbox 2 added

---
+++ src/helpers/TextBoxTwo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TextBox = () => {
+  return (
+    <div className='text-box'>
+        <h3>Feeling Disconnected</h3>
+        <p>
+            'Feeling Disconnected' in an awtwork that represents how out of touch we are to the real world.
+            
+        </p>
+    </div>
+  )
+}
+
+export default TextBox
