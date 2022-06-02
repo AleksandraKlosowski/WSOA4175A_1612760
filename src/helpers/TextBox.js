85bc4d023790a31784f1ea8779a69bd060f73ce5
AleksandraKlosowski <1612760@students.wits.ac.za>
2022-06-02 21:46:28 +0200

    txt box 1

---
+++ src/helpers/TextBox.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TextBox = () => {
+  return (
+    <div className='text-box'>
+        <h3>The Technological world</h3>
+        <p>
+            Ironically in a world that focuses on creating the luxury of connectivity, 
+            with tools that assist in being able to connect and communicate from across the globe, 
+            we have become the most 'disconnected' generation. In connecting via socially media, we have
+            become ever so anti-social in the real world.
+        </p>
+    </div>
+  )
+}
+
+export default TextBox
