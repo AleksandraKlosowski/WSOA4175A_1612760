adeec44f9121595176e42a7c282b7cd91d849beb
AleksandraKlosowski <1612760@students.wits.ac.za>
2022-06-02 23:32:25 +0200

    preparatory work

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import AboutImage from '../assets/img8.jpg';
 import "../styles/About.css";
 import {WireframeList} from '../helpers/WireframeList';
+import {WireframeListTwo} from '../helpers/WireframeListTwo';
 import WireframeItem from '../components/WireframeItem';
+import Inspo from '../img/inspo.jpg';
+import Web from '../img/web.jpg';
+import Web2 from '../img/web2.png';
+import Web3 from '../img/web3.png';
 
 
 export default class About extends Component {
@@ -33,7 +38,48 @@ export default class About extends Component {
             })}
           </div>
           </div>
+          <div className='aboutTop'>
+          <h1>Preperatory Work - Inspirations </h1>
+          <h5>Website Aesthetic</h5>
+          </div>
+          <div className="aboutBottom2">
+              <p>I have chosen to go for a simple and clean aesthetic. I have done this as 
+                my website will be displaying other works and advertising my digital art 
+                capabilities throughout the page. By having a relatively neutral website, 
+                I can display vibrant and creative art that does not contrast and clash 
+                with the overall design of the website. Neutral, elegant and quirky websites
+                 allow for greater attraction from various individuals. Should I choose to 
+                 do a loud and vibrant website, it can be off-putting for more conservative 
+                 viewers. Despite being neutral, it is equally engaging for the younger 
+                 audience as it is currently trendy.
+              </p>
+          </div>
+          <h5>simple. elegant. classy.</h5>
+          <img src={Inspo} alt="inspo" class = "center img1"/>
+          <h1>Wireframes</h1>
+          <div className='wireframe'>
+          <div className="wireframeList">
+            {WireframeListTwo.map((wireframeItem, value) => {
+                return(
+                    <WireframeItem value ={value} name ={wireframeItem.name} price ={wireframeItem.price} image ={wireframeItem.image}/>
+                )
+            })}
+          </div>
+          </div>
+          <h5>Web Art</h5>
+          <div className="aboutBottom2">
+              <p>My goal is to create a visual representation of being disconnected.  
+                In our technological age, we have become more reliant on technological 
+                communicated and become more accustomed to remaining in the comfort of 
+                our own home, this has been further promoted by Covid 19.
+              </p>
+          </div>
+          <img src={Web} alt="web" class = "center img2"/>
+          <img src={Web2} alt="web2" class = "center img2"/>
+          <img src={Web3} alt="web3" class = "center img2"/>
       </div> 
+
+      
     )
   }
 }
