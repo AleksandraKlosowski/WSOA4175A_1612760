50114f97e4c5d6ee3fd0de31e4f4a6c99d879b9b
AleksandraKlosowski <1612760@students.wits.ac.za>
2022-06-03 13:15:08 +0200

    updated wireframe list

--- src/helpers/WireframeListTwo.js
+++ src/helpers/WireframeListTwo.js
@@ -1,6 +1,7 @@
 import Wireframe5 from '../assets/Wireframe5.png';
 import Wireframe6 from '../assets/Wireframe6.png';
 import Wireframe7 from '../assets/Wireframe7.png';
+import Wireframe8 from '../assets/Wireframe8.png';
 
 
 
@@ -16,6 +17,10 @@ export const WireframeListTwo = [
     {
         name: "wireframe7",
         image: Wireframe7,
+    },
+    {
+        name: "wireframe8",
+        image: Wireframe8,
     },
 
 ];
